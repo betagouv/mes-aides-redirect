6fc6cbb38388b32d2f5c379626bd0504f3543c93
Thomas Guillet <thomas@palya.fr>
2021-12-01 18:45:03 +0100

    Redirige tout le trafic vers le PNDS

--- next.config.js
+++ next.config.js
@@ -1,3 +1,36 @@
 module.exports = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+/*      {
+        source: '/',
+        destination: '/local',
+        permanent: false,
+        has: [
+          {
+            type: 'header',
+            key: 'referer',
+            value: '.*localhost.*',
+          },
+        ],
+      },
+      {
+        source: '/',
+        destination: '/ameli',
+        permanent: false,
+        has: [
+          {
+            type: 'header',
+            key: 'referer',
+            value: '.*ameli.*',
+          },
+        ],
+      },*/
+      {
+        source: '/',
+        destination: 'https://www.mesdroitssociaux.gouv.fr?utm_source=mes-aides&utm_medium=basic',
+        permanent: false,
+      },
+    ]
+  },
 }
